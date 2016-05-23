638554bbbe150b48a7f96949f95adc504c2f0b57
Aseem Dua <dua.bitsp@gmail.com>
2016-05-23 15:01:28 +0200

    Updated tests. Assert check unclear

--- test/test.py
+++ test/test.py
@@ -1,11 +1,49 @@
 import blocklinalg
+from scipy.sparse import coo_matrix, csc_matrix
 
 def test0():
-    # from scipy.sparse import coo_matrix, csc_matrix
-    # A = coo_matrix([[1, 2], [3, 4]])
-    # B = coo_matrix([[5],[6]]) 
-    # C = csc_matrix([[7]])
-    # BM = blocklinalg.BlockMatrix([[A, B], [None, C]])
+    
+    A = coo_matrix([[1, 2], [3, 4]])
+    B = coo_matrix([[5],[6]]) 
+    C = csc_matrix([[7]])
+    
+    Ab = blocklinalg.Block(A)
+    Bb = blocklinalg.Block(B)
+    Cb = blocklinalg.Block(C)
+    Db = blocklinalg.Block(None)
+    
+    assert isinstance(Ab, blocklinalg.Block)
+    
+     ### Following tests work perfectly. Not sure how to test using assert ###
+
+    S = Ab+Ab 
+    # assert S[0,0] == 2 
+    # assert S[0,1] == 4
+    # assert S[1,0] == 6
+    # assert S[1,1] == 8
+
+    T  = Ab.transpose()
+    # assert T[0,0] == 1
+    # assert T[0,1] == 3
+    # assert T[1,0] == 2
+    # assert T[1,1] == 4
+
+    M = Ab*Ab
+    # assert M[0,0] == 1
+    # assert M[0,1] == 4
+    # assert M[1,0] == 9
+    # assert M[1,1] == 16
+
+    Dot = Ab.dot(Bb)
+    assert Dot.shape == (2,1)
+    # assert Dot[0,0] == 17
+    # assert Dot[1,0] == 39
+
+    P = blocklinalg.BlockMatrix([[A, B], [None, C]])
+    assert isinstance(P, blocklinalg.BlockMatrix)
+    Psum = P + P
+    assert P.shape == Psum.shape
+    assert P.msize == Psum.msize
 
     # @aseem Insert some code here that you want to work, e.g.,
     #
