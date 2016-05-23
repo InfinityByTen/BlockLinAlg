cca65bc9499dd49b6128c64ecf3713cf8d5b5097
Aseem Dua <dua.bitsp@gmail.com>
2016-05-23 15:02:34 +0200

    Forced block form in BlockMatrix for consistency

--- blocklinalg/__init__.py
+++ blocklinalg/__init__.py
@@ -11,7 +11,11 @@ class Block(object):
             self.shape = matrix.shape
         else:
             self.shape = None
-        self.matrix  = matrix
+
+        if isinstance(matrix,np.ndarray):
+            self.matrix = coo_matrix(matrix)
+        else:
+            self.matrix  = matrix
 
     def __str__(self):
         return self.matrix.__str__()
@@ -20,6 +24,9 @@ class Block(object):
         return " Block : " + self.matrix.__repr__()
 
     def __add__(self, other):
+
+        if isinstance(other,Block) is False:
+            raise TypeError('Both Matrices should be of Block type')
 
         if self.matrix is not None and other.matrix is not None:
             return Block(self.matrix + other.matrix)
@@ -30,13 +37,13 @@ class Block(object):
 
     def __mul__(self, other):
         
+        if isinstance(other,Block) is False:
+            raise TypeError('Both Matrices should be of Block type')
+
         if self.matrix is not None and other.matrix is not None:
-            print "self has shape", self.matrix.shape
-            print "other has shape", other.matrix.shape
-            if self.matrix.shape[1]==other.matrix.shape[0]:
-                return Block(self.matrix.dot(other.matrix))
-            else:
-                raise ValueError('dimension mismatch')
+            # print "self has shape", self.matrix.shape
+            # print "other has shape", other.matrix.shape
+            return Block(self.matrix.dot(other.matrix))
         else:
             return None
 
@@ -48,6 +55,9 @@ class Block(object):
             raise AttributeError('Given Block does not have attribute', attr)
 
     def dot(self, other):
+
+        if isinstance(other,Block) is False:
+            raise TypeError('Both Matrices should be of Block type')
 
         if self.matrix is not None and other.matrix is not None:
             return Block(self.matrix.dot(other.matrix))
@@ -61,10 +71,10 @@ class BlockMatrix(np.ndarray):
 
     def __new__(cls,mblocks):
         # print type(mblocks)
-        obj = np.asarray(mblocks).view(cls)
+        obj = np.asarray(mblocks)
         if obj.ndim !=2:
             raise ValueError('BlockMatrix must be 2-D. Kindly use BlockVector class for 1-D')
-        return obj
+        return obj.view(cls)
 
     def __str__(self):
         return str(self.__array__())
@@ -78,26 +88,25 @@ class BlockMatrix(np.ndarray):
         # print "Self is of type", type(self)
         # print "Self has shape", self.shape
 
-        # for i in range(obj.shape[0]):
-        #     for j in range(obj.shape[1]):
-        #       if isinstance(obj[i,j],Block) is False:
-        #           obj[i,j] = Block(obj[i,j])  
+        for i in range(obj.shape[0]):
+            for j in range(obj.shape[1]):
+              if isinstance(obj[i,j],Block) is False and isinstance(obj[i,j], BlockMatrix) is False:
+                  obj[i,j] = Block(obj[i,j])
         
         self.Row_Length = np.zeros(self.shape[0], dtype=np.int64)
         self.Col_Length = np.zeros(self.shape[1], dtype=np.int64)
 
-        print"Checking Validity"
+        # print"Checking Validity"
 
         for i in range(obj.shape[0]):
             for j in range(obj.shape[1]):
-                if obj[i,j] is not None:
+                if obj[i,j].matrix is not None:
                     if self.Row_Length[i] == 0:
-                        if isinstance(obj[i,j], (spmatrix,Block)):
-                            self.Row_Length[i] = obj[i,j].shape[0]
-                        elif isinstance(obj[i,j],np.ndarray) and isinstance(obj[i,j],BlockMatrix):
+                        if isinstance(obj[i,j], Block):
+                            if obj[i,j].matrix is not None:
+                                self.Row_Length[i] = obj[i,j].shape[0]
+                        elif isinstance(obj[i,j],BlockMatrix):
                             self.Row_Length[i] = sum(obj[i,j].Row_Length)
-                        elif isinstance(obj[i,j],np.ndarray):
-                            self.Row_Length[i] = coo_matrix(obj[i,j]).shape[0]
                         else:
                             raise ValueError('blocks must be either sparse matrix or BlockMatrix objects')
                     else:
@@ -105,12 +114,11 @@ class BlockMatrix(np.ndarray):
                             raise ValueError('blocks[%d,:] has incompatible row dimensions' % i)
                         
                     if self.Col_Length[j] == 0:
-                        if isinstance(obj[i,j], (spmatrix,Block)):
-                            self.Col_Length[j] = obj[i,j].shape[1]
-                        elif isinstance(obj[i,j],np.ndarray) and isinstance(obj[i,j],BlockMatrix):
+                        if isinstance(obj[i,j], Block):
+                            if obj[i,j].matrix is not None:
+                                self.Col_Length[j] = obj[i,j].shape[1]
+                        elif isinstance(obj[i,j],BlockMatrix):
                             self.Col_Length[i] = sum(obj[i,j].Col_Length)
-                        elif isinstance(obj[i,j],np.ndarray):
-                            self.Col_Length[i] = coo_matrix(obj[i,j]).shape[0]
                         else:
                             raise ValueError('blocks must be either sparse matrix or BlockMatrix objects')
                     else:
@@ -124,8 +132,9 @@ class BlockMatrix(np.ndarray):
         if self.Col_Length.min() == 0:
             raise ValueError('blocks[:,%d] is all None' % self.Col_Length.argmin())
 
-        print "Validity Check complete"
-
-        # Check Validity 
+        # print "Validity Check complete"
+        
+        self.msize = (sum(self.Row_Length),sum(self.Col_Length))
+        
         if obj is None:
             return None
